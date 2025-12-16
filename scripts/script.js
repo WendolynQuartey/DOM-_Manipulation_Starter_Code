be4288ef2c7702023f84994abd2bb6c1fbd94400
WendolynQuartey <quarteywendolyn@gmail.com>
2025-12-16 15:38:31 -0500

    part 1 and part 2 complete

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,14 @@ mainEl.style.backgroundColor = bg;
 mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
 
 mainEl.classList.add("flex-ctr");
+
+et menuBg = 'var(--top-menu-bg)';
+
+let topMenuEl = document.getElementById("top-menu");
+
+
+
+topMenuEl.style.height = "100%";
+
+topMenuEl.style.backgroundColor = menuBg;
+topMenuEl.classList.add('flex-around');
