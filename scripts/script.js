b395d852bbab182fdd376d5d5427afd046fab990
WendolynQuartey <quarteywendolyn@gmail.com>
2025-12-17 22:03:17 -0500

    Part 4 complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,76 @@
 let menuLinks = [
   { text: 'about', href: '/about' },
-  { text: 'catalog', href: '/catalog' },
-  { text: 'orders', href: '/orders' },
-  { text: 'account', href: '/account' },
+  {
+    text: 'catalog', href: '#', subLinks: [
+      { text: 'all', href: '/catalog/all' },
+      { text: 'top selling', href: '/catalog/top' },
+      { text: 'search', href: '/catalog/search' },
+    ]
+  },
+  {
+    text: 'orders', href: '#', subLinks: [
+      { text: 'new', href: '/orders/new' },
+      { text: 'pending', href: '/orders/pending' },
+      { text: 'history', href: '/orders/history' },
+    ]
+  },
+  {
+    text: 'account', href: '#', subLinks: [
+      { text: 'profile', href: '/account/profile' },
+      { text: 'sign out', href: '/account/signout' },
+    ]
+  },
 ]
 
-let mainEl = document.querySelector("main");
-let bg = 'var(--main-bg)';
+let bg = 'var(--main-bg)';
+let menuBg = 'var(--top-menu-bg)';
+let subMenuBg = 'var(--sub-menu-bg)'
+
+
+let mainEl = document.querySelector('main');
 
 mainEl.style.backgroundColor = bg;
-
-mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
-
-mainEl.classList.add("flex-ctr");
-
-let menuBg = 'var(--top-menu-bg)';
-
-let topMenuEl = document.getElementById("top-menu");
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+mainEl.classList.add('flex-ctr');
 
 
+let topMenuEl = document.getElementById('top-menu');
 
-topMenuEl.style.height = "100%";
-
+topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = menuBg;
 topMenuEl.classList.add('flex-around');
 
-for(let i of menuLinks){
-   let a = document.createElement("a");
-   a.getAttribute(i.href);
-   a.textContent = i.text;
-   topMenuEl.appendChild(a);
+for (let i of menuLinks) {
+  let a = document.createElement('a');
+  a.getAttribute(i.href);
+  a.textContent = i.text;
+  topMenuEl.appendChild(a);
 }
+
+let subMenuEl = document.getElementById('sub-menu');
+subMenuEl.style.height = '100%';
+subMenuEl.style.backgroundColor = subMenuBg;
+subMenuEl.classList.add('flex-around');
+subMenuEl.style.position = 'absolute';
+subMenuEl.style.top = '0';
+
+let topMenuLinks = topMenuEl.querySelectorAll('a');
+// console.log(topMenuLinks.textContent);
+topMenuEl.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  // console.log(event.target.tagName);
+  if (event.target.tagName !== 'A') return;
+  // console.log("testing!");
+  // console.log(event.target.tagName);
+  console.log(event.target.textContent);
+  for (let i = 0; i < topMenuLinks.length; i++) {
+    topMenuLinks[i].classList.remove('active');
+  }
+  
+  event.target.classList.add('active');
+
+  // event.target.classList.remove('active');
+}
+
